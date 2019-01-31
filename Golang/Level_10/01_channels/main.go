b38ce65152c60df67f592dd27b0a4d72c964fab5
Oscar Piragauta <oscarl1718@gmail.com>
2019-01-30 22:30:21 -0500

    Level 10

---
+++ Golang/Level_10/01_channels/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	withBuffer()
+	withFuncLiteral()
+}
+
+func withBuffer() {
+	c := make(chan int, 1)
+	c <- 42
+	fmt.Println(<-c)
+}
+
+func withFuncLiteral() {
+	c := make(chan int)
+	go func() {
+		c <- 42
+	}()
+	fmt.Println(<-c)
+}
