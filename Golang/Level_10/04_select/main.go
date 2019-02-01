3c28dd44c49617825f7bc5d3c44bdfd815ec0f7f
Oscar Piragauta <oscarl1718@gmail.com>
2019-02-01 08:10:32 -0500

    Channels wiith select statemen

--- Golang/Level_10/04_select/main.go
+++ Golang/Level_10/04_select/main.go
@@ -5,22 +5,30 @@ import "fmt"
 func main() {
 	q := make(chan int)
 	c := gen(q)
-
 	receive(c, q)
-
 	fmt.Println("about to exit")
 }
 
 func receive(c <-chan int, q chan int) {
-
+	for {
+		select {
+		case v := <-c:
+			fmt.Println(v)
+		case <-q:
+			return
+		}
+	}
 }
 
-func gen(q <-chan int) <-chan int {
+func gen(q chan<- int) <-chan int {
 	c := make(chan int)
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+		q <- 1
 
-	for i := 0; i < 100; i++ {
-		c <- i
-	}
-
+	}()
 	return c
 }
